07b503430be82093443bc1ad9d2c88141d6f7196
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-12-08 11:44:24 +0100

    Resumen

---
+++ Primeros_pasos/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace Resumen
+{
+    //PLANTILLA de datos más que lógicos.Persona, Animal, Figura...
+    //Define GENERALMENTE como son las personas...
+             
+    class Class
+    {
+    }
+}
--- Primeros_pasos/Program.cs
+++ Primeros_pasos/Program.cs
@@ -1,17 +1,14 @@
-﻿namespace Primeros_pasos
+﻿namespace Resumen
 {
+    //Instanciar una clase, creando un OBJETO DE LA PLANTILLA de la CLASE
+    //Define CONCRETAMENTE como son las personas... ->  Cristina López, de 37 años 
+
+    //NombreClase nombre = new NombreClase(); 
     class Program
     {
         static void Main()
         {
-            /*
-             * Clase -> Plantilla de datos más que lógicos. Persona, Animal, Figura... 
-                        Define generalmente como son las personas...
-             * Program -> Instanciar una clase, creando un objeto de la plantilla de la clase. 
-                          Persona persona = new Persona(); ->  Cristina López, de 37 años.  
-                          Define concretamente como son las personas...
-             * 
-             */
+            
         }
     }
 }
